6026e39c2ed55e1907a41649f842a6d79e56526c
Ivan Schwindt <ynventa@hotmail.com>
2018-03-27 09:10:31 -0300

    Change location of url_helpers

--- app/serializers/city_serializer.rb
+++ app/serializers/city_serializer.rb
@@ -9,9 +9,10 @@
 #  updated_at  :datetime         not null
 #  province_id :integer
 #
-include Rails.application.routes.url_helpers
 
 class CitySerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers
+  
   attributes :id, :name, :region_name, :province_name, :url
 
   def region_name
