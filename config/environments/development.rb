459c3fabcb4cc900a04aadda64092344ce8c0970
Ivan Schwindt <ynventa@hotmail.com>
2018-05-25 02:11:43 -0300

    Configure email for development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,6 +47,18 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'patagonia.viajera@gmail.com',
+    password:             'abril1234',
+    authentication:       'plain',
+    enable_starttls_auto: true  
+  }
+
+
   config.i18n.available_locales = [:es, :en]
   config.i18n.default_locale = :es
   
